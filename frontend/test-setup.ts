2725ac3eafc6e28488a532eb01fae81fa9251fbc
Eric So <me@eric.so>
2025-05-16 13:04:02 -0700

    trying a different crypto lib for the ployfill

--- frontend/test-setup.ts
+++ frontend/test-setup.ts
@@ -1,27 +1,16 @@
-import { webcrypto } from 'node:crypto'
+import { Crypto } from '@peculiar/webcrypto';
 
-// Create a more complete crypto polyfill
-const cryptoPolyfill = {
-    ...webcrypto,
-    getRandomValues: function(buffer) {
-        return webcrypto.getRandomValues(buffer);
-    }
-};
+const crypto = new Crypto();
 
-// Ensure crypto is available globally
-if (!global.crypto) {
-    Object.defineProperty(global, 'crypto', {
-        value: cryptoPolyfill,
-        writable: false,
-        configurable: false
-    });
-}
+// Ensure crypto is available globally with all required methods
+Object.defineProperty(global, 'crypto', {
+    value: crypto,
+    writable: false,
+    configurable: false
+});
 
-// Also ensure it's available on globalThis
-if (!globalThis.crypto) {
-    Object.defineProperty(globalThis, 'crypto', {
-        value: cryptoPolyfill,
-        writable: false,
-        configurable: false
-    });
-}
+Object.defineProperty(globalThis, 'crypto', {
+    value: crypto,
+    writable: false,
+    configurable: false
+});
